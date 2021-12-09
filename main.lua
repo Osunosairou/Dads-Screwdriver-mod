5bca6e857fbd8fcc8a2dbfc76f32061fdc528087
Osunosairou <theonduarte@gmail.com>
2021-12-09 20:40:15 -0300

    created the main files

---
+++ main.lua
@@ -0,0 +1,19 @@
+local Mod = RegisterMod("Dad's Screwdriver", 1)
+local game = Game()
+local screwSwap
+
+TrinketType.TRINKET_SCREWDRIVER = Isaac.GetTrinketIdByName("Dad's Screwdriver")
+
+function Mod:onUpdate(player)
+    if game:GetFrameCount() == 1 then
+        local roll = player:GetTrinketRNG(TrinketType.TRINKET_SCREWDRIVER)
+        screwSwap = roll:RandomInt(TrinketType.NUM_TRINKETS)
+    end
+
+    for _, entity in pairs(Isaac.GetRoomEntities()) do
+        if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_TRINKET and entity.SubType == screwSwap then
+            entity.SubType = TrinketType.TRINKET_SCREWDRIVER
+        end
+    end
+end
+Mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, Mod.onUpdate)
