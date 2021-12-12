c5f8184c45c81003512ba1f6dd7115be31a50d47
Osunosairou <theonduarte@gmail.com>
2021-12-12 15:34:02 -0300

    changed a variable name

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
 local Mod = RegisterMod("Dad's Screwdriver", 1)
 local game = Game()
-
-
-TrinketType.TRINKET_SCREWDRIVER = Isaac.GetTrinketIdByName("Dad's Screwdriver")
+local screwId = Isaac.GetTrinketIdByName("Dad's Screwdriver")
 
 function Mod:onUpdate(player)
     if game:GetFrameCount() == 1 then
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, TrinketType.TRINKET_SCREWDRIVER, Vector(320,300), Vector(0,0), nil)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, screwId, Vector(320,300), Vector(0,0), nil)
     end
 
 end
