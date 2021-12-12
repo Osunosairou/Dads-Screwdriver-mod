d422ed10e52d06812e07ef707e9adfa80a56e9d5
Osunosairou <theonduarte@gmail.com>
2021-12-12 15:32:39 -0300

    code to make it spawn on the room

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ TrinketType.TRINKET_SCREWDRIVER = Isaac.GetTrinketIdByName("Dad's Screwdriver")
 
 function Mod:onUpdate(player)
     if game:GetFrameCount() == 1 then
-        
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, TrinketType.TRINKET_SCREWDRIVER, Vector(320,300), Vector(0,0), nil)
     end
 
     
